101dbda3e8582b474f04b0c81a6ebe0d661efb65
Korey Hermann <khermann135@gmail.com>
2023-02-22 15:25:40 -0500

    added more code and employee data

--- index.js
+++ index.js
@@ -35,3 +35,92 @@ const generateManager = () => {
         empArray.push(manager);
     })
 };
+
+const generateEmployee = () => {
+return inquirer.prompt ([
+    {
+        type: 'list',
+        name: 'role',
+        message: "Select employee role:",
+        choices: ['Engineer', 'Intern']
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: "What's the name of the employee?", 
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Please enter the employee's ID.",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Please enter the employee's email.",
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: "Please enter the employee's github username.",
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: "Please enter the intern's school",
+    },
+    {
+        type: 'confirm',
+        name: 'confirmAddEmployee',
+        message: 'Would you like to add another employee?',
+        default: false
+    }
+])
+.then(employeeData => {
+
+    let { name, id, email, role, github, school, confirmAddEmployee } = employeeData; 
+    let employee; 
+
+    if (role === "Engineer") {
+        employee = new Engineer (name, id, email, github);
+
+        console.log(employee);
+
+    } else if (role === "Intern") {
+        employee = new Intern (name, id, email, school);
+
+        console.log(employee);
+    }
+
+    empArray.push(employee); 
+
+    if (confirmAddEmployee) {
+        return addEmployee(empArray); 
+    } else {
+        return empArray;
+    }
+})
+};
+
+const writeFile = data => {
+    fs.writeFile('dist\index.html', data, err => {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log("Team profiles created")
+        }
+    })
+};
+
+generateManager()
+  .then(generateEmployee)
+  .then(empArray => {
+    return generateHTML(teamArray);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .catch(err => {
+ console.log(err);
+  });
